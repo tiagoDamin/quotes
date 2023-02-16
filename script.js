0489ffd8f5bfac56a3bddbf071d7b09d9bfa6c27
tiagoDamin <tiago_tb22@hotmail.com>
2023-02-16 16:31:35 -0300

    quote generator

---
+++ script.js
@@ -0,0 +1,85 @@
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
+const loader = document.getElementById('loader');
+
+// quote generator logic
+let apiQuotes = []; 
+
+// show loading
+function loading() {
+    loader.hidden = false;
+    quoteContainer.hidden = true;
+}
+
+function complete() {
+    quoteContainer.hidden = false;
+    loader.hidden = true;
+}
+
+// Show new quote
+function newQuote() {
+    loading();
+    // Pick a random quote from apiQuotes array
+    const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+    
+
+    // Check if Author field is blank and replace it with 'Uknown'
+    if(!quote.author) {
+        authorText.textContent = 'Uknown'
+    } else {
+        // Show quote author html
+        authorText.textContent = quote.author;
+    }
+
+    // Check Quote lenght to determine styling
+    if(quote.text.length > 20) {
+        quoteText.classList.add('long-quote')
+    } else {
+        quoteText.classList.remove('long-quote')
+    }
+
+    // Show quote htnml
+    quoteText.textContent = quote.text;
+    complete();
+
+}
+
+
+
+
+// Get a quote rom API
+async function getQuotes() {
+    loading();
+    const apiUrl = 'https://type.fit/api/quotes';
+
+    try {
+
+        const response = await fetch(apiUrl);
+        apiQuotes = await response.json();
+     newQuote();
+    } catch (error) {
+        // Catch Error Here
+
+    }
+}
+
+
+// Tweet quote button
+function tweetQuote() {
+    const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+    window.open(twitterUrl, '__blank');
+}
+
+
+// Event listeners
+
+// new quote button
+newQuoteBtn.addEventListener('click', newQuote);
+
+// twitter button
+twitterBtn.addEventListener('click', tweetQuote)
+
+getQuotes();
